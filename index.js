9d727b522d3a394effaeea6e4309c04452e096af
Ben <bennaat@gmail.com>
2024-05-13 14:25:04 +0700

    exercise 4.1 fist commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let product = {
+    product_id: 1,
+    product_name: 'Fanta',
+    quantity_in_stock: 51,
+    unit_price: 15,
+    product_description: {
+        product_brand: 'Fantasy',
+        size_or_quantity: 25,
+        tast_or_scent: 'sour',
+        colour: 'blue',
+        additional_description: ['new 2024'],
+},
+}
+
+console.log(product);
